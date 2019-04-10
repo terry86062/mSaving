2cfca73e3bfbbab04dc3b57dc5999892a37e7fdb
Terry <terry86062@gmail.com>
2019-04-11 00:57:13 +0800

    Build segue to account detail view controller

--- mSaving/Controller/Setting/AccountDetailVC.swift
+++ mSaving/Controller/Setting/AccountDetailVC.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class AccountDetailVC: UIViewController {
-
+    
+    @IBOutlet weak var accountTextField: UITextField!
+    
+    @IBOutlet weak var accountAmountTextField: UITextField!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        accountTextField.becomeFirstResponder()
 
     }
 
+    @IBAction func dismiss(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+        hideTabBarVCBlackView()
+        
+    }
+    
+    @IBAction func confirm(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+        hideTabBarVCBlackView()
+        
+    }
+    
+    func hideTabBarVCBlackView() {
+        
+        guard let tabBarVC = presentingViewController as? TabBarController else { return }
+        
+        tabBarVC.blackView.isHidden = true
+        
+    }
+    
 }
