1a5bd95142f52672ead855a7ee0bc0bd1d991eef
Terry <terry86062@gmail.com>
2019-04-19 14:46:36 +0800

    UI - set pie chart color

--- mSaving/View/Chart/PieChartCVCell.swift
+++ mSaving/View/Chart/PieChartCVCell.swift
@@ -32,20 +32,19 @@ class PieChartCVCell: UICollectionViewCell {
             
         }
         
-        let entry1 = PieChartDataEntry(value: 10, label: "#1")
-        let entry2 = PieChartDataEntry(value: 5, label: "#2")
-        let entry3 = PieChartDataEntry(value: 20, label: "#3")
-        let entry4 = PieChartDataEntry(value: 30, label: "#4")
-        let entry5 = PieChartDataEntry(value: 15, label: "#5")
-        let entry6 = PieChartDataEntry(value: 25, label: "#6")
-        let entry7 = PieChartDataEntry(value: 40, label: "#7")
-        let entry8 = PieChartDataEntry(value: 2, label: "#8")
-        
-//        let dataSet = PieChartDataSet(values: [
-//            entry1, entry2, entry3, entry4, entry5, entry6, entry7, entry8
-//            ], label: "Widget Types")
-        
         let dataSet = PieChartDataSet(values: dataArray, label: "")
+        
+        var colors: [NSUIColor] = []
+        
+        for index in 0...categoryAccountingMonthTotals.count - 1 {
+
+            guard let color = categoryAccountingMonthTotals[index].expenseCategory.color else { return }
+
+            colors.append(UIColor.hexStringToUIColor(hex: color))
+
+        }
+        
+        dataSet.colors = colors
         
         let data = PieChartData(dataSet: dataSet)
         
@@ -54,7 +53,7 @@ class PieChartCVCell: UICollectionViewCell {
 
         //All other additions to this function will go here
 
-        dataSet.colors = ChartColorTemplates.joyful()
+//        dataSet.colors = ChartColorTemplates.joyful()
 
         //This must stay at end of function
         pieChart.notifyDataSetChanged()
