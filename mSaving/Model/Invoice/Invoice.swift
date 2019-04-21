8d590782363e17ce9b48fbbf65cf3df07ee73ee3
Terry <terry86062@gmail.com>
2019-04-22 00:53:27 +0800

    Func - create Invoice struct

---
+++ mSaving/Model/Invoice/Invoice.swift
@@ -0,0 +1,53 @@
+//
+//  Invoice.swift
+//  mSaving
+//
+//  Created by 黃偉勛 Terry on 2019/4/22.
+//  Copyright © 2019 Terry. All rights reserved.
+//
+
+import Foundation
+
+struct Invoice: Codable {
+    
+    let msg: String
+    
+    let code: String
+    
+    let invNum: String
+    
+    let invoiceTime: String
+    
+    let invStatus: String
+    
+    let sellerName: String
+    
+    let invPeriod: String
+    
+    let sellerAddress: String
+    
+    let sellerBan: String
+    
+    let buyerBan: String
+    
+    let currency: String
+    
+    let details: [Goods]
+    
+    let invDate: String
+    
+}
+
+struct Goods: Codable {
+    
+    let unitPrice: String
+    
+    let amount: String
+    
+    let quantity: String
+    
+    let rowNum: String
+    
+    let description: String
+    
+}
