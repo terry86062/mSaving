1b5aa5f93a21c080111cd9f422089a6e2d1e0d8f
Terry <terry86062@gmail.com>
2019-04-22 16:54:43 +0800

    Func - create a invoice to accounting view controller

---
+++ mSaving/Controller/Invoice/InvoiceToAccountingVC.swift
@@ -0,0 +1,19 @@
+//
+//  InvoiceToAccountingVC.swift
+//  mSaving
+//
+//  Created by 黃偉勛 Terry on 2019/4/22.
+//  Copyright © 2019 Terry. All rights reserved.
+//
+
+import UIKit
+
+class InvoiceToAccountingVC: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+    }
+
+}
