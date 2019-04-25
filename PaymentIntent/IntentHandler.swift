226ef32c8174f5cb8f6bbf96bb4cb8742ed2b9aa
Terry <terry86062@gmail.com>
2019-04-25 13:55:50 +0800

    Func - create an add accounting label on intent view controller to show amount

--- PaymentIntent/IntentHandler.swift
+++ PaymentIntent/IntentHandler.swift
@@ -8,32 +8,34 @@
 
 import Intents
 
-class IntentHandler: INExtension {
-    
-    override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        
-        return self
-    }
-    
-}
+class IntentHandler: INExtension { }
 
 extension IntentHandler: INPayBillIntentHandling {
 
     func handle(intent: INPayBillIntent, completion: @escaping (INPayBillIntentResponse) -> Void) {
         
         guard let amount = intent.transactionAmount?.amount?.amount?.doubleValue else {
+            
             completion(INPayBillIntentResponse(code: .failure, userActivity: nil))
+            
             return
+            
         }
 
         guard let category = CategoryProvider().expenseCategory else {
+            
             completion(INPayBillIntentResponse(code: .failure, userActivity: nil))
+            
             return
+            
         }
 
-        AccountingProvider().saveAccounting(date: Date(), amount: Int64(amount), accountName: "現金", selectedExpenseCategory: category[0], selectedIncomeCategory: nil, selectedExpense: true)
+        AccountingProvider().saveAccounting(date: Date(),
+                                            amount: Int64(amount),
+                                            accountName: "現金",
+                                            selectedExpenseCategory: category[0],
+                                            selectedIncomeCategory: nil,
+                                            selectedExpense: true)
 
         completion(INPayBillIntentResponse(code: .success, userActivity: nil))
 
--- PaymentIntentUI/IntentViewController.swift
+++ PaymentIntentUI/IntentViewController.swift
@@ -8,43 +8,45 @@
 
 import IntentsUI
 
-class IntentViewController: UIViewController, INUIHostedViewControlling {
+class IntentViewController: UIViewController {
     
     @IBOutlet weak var addAccountingLabel: UILabel!
+    
+    var desiredSize: CGSize {
+        
+        return CGSize(width: 320, height: 150)
+        
+    }
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
     }
-        
-    // MARK: - INUIHostedViewControlling
+
+}
+
+extension IntentViewController: INUIHostedViewControlling {
     
-    // Prepare your view controller for the interaction to handle.
     func configureView(for parameters: Set<INParameter>,
                        of interaction: INInteraction,
                        interactiveBehavior: INUIInteractiveBehavior,
                        context: INUIHostedViewContext,
                        completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void) {
         
-        // Do configuration here, including preparing views and calculating a desired size for presentation.
+        guard let inPayBillIntent = interaction.intent as? INPayBillIntent,
+            let amount = inPayBillIntent.transactionAmount?.amount?.amount?.doubleValue else {
+            
+            addAccountingLabel.text = "金額讀取錯誤"
+                
+            return
+            
+        }
+        
+        addAccountingLabel.text = "記錄食物花費\(amount)元"
         
         completion(true, parameters, self.desiredSize)
         
     }
-    
-    var desiredSize: CGSize {
-        
-//        return self.extensionContext!.hostedViewMaximumAllowedSize
-        
-        return CGSize(width: 320, height: 150)
-        
-    }
-    
-}
-
-extension IntentViewController {
-    
-    
     
 }
